f38c6a8ee3eabd0927c1ef3bb86fb5ad64d1ce9d
Kim T Henriksen <kim.t.henriksen@gmail.com>
2026-07-24 12:57:56 +0200

    Add cart quantity buttons

--- js/cart.js
+++ js/cart.js
@@ -14,18 +14,45 @@ cart.forEach((product) => {
   const image = document.createElement("img");
   image.src = product.image.url;
   image.alt = product.image.alt;
-
   cartItem.appendChild(image);
 
   const title = document.createElement("h2");
   title.textContent = product.title;
-
   cartItem.appendChild(title);
 
   const price = document.createElement("p");
   price.textContent = `$${product.price}`;
-
   cartItem.appendChild(price);
+
+  const quantity = document.createElement("p");
+  quantity.textContent = `Quantity: ${product.quantity}`;
+  cartItem.appendChild(quantity);
+
+  const decreaseButton = document.createElement("button");
+  decreaseButton.textContent = "-";
+  cartItem.appendChild(decreaseButton);
+
+  const increaseButton = document.createElement("button");
+  increaseButton.textContent = "+";
+  cartItem.appendChild(increaseButton);
+
+  increaseButton.addEventListener("click", () => {
+    product.quantity++;
+
+    quantity.textContent = `Quantity: ${product.quantity}`;
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+  });
+
+  decreaseButton.addEventListener("click", () => {
+    if (product.quantity > 1) {
+      product.quantity--;
+
+      quantity.textContent = `Quantity: ${product.quantity}`;
+
+      localStorage.setItem("cart", JSON.stringify(cart));
+    }
+  });
 
   cartItems.appendChild(cartItem);
 });

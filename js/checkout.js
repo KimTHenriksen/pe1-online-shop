834769cbc564601f6ba94bfcff6b1d07138cc96f
Kim T Henriksen <kim.t.henriksen@gmail.com>
2026-08-01 12:49:03 +0200

    Adjust checkout page

--- js/checkout.js
+++ js/checkout.js
@@ -1,9 +1,16 @@
 const paymentForm = document.getElementById("payment-form");
+const message = document.querySelector(".message");
+const paypal = document.getElementById("paypal");
+const googlePay = document.getElementById("google-pay");
+const creditCard = document.getElementById("credit-card");
 
 paymentForm.addEventListener("submit", function (event) {
   event.preventDefault();
 
-  console.log("submitted");
+  if (!paypal.checked && !googlePay.checked && !creditCard.checked) {
+    message.textContent = "Select a payment method";
+    return;
+  }
 
   localStorage.removeItem("cart");
 
